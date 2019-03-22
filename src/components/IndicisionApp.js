cd05f9f856eaa28a40f15d43321fcba2bf7310f8
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-22 16:34:55 -0600

    Wrapped Action,Options,AddOption w/ container

--- src/components/IndicisionApp.js
+++ src/components/IndicisionApp.js
@@ -77,13 +77,15 @@ class IndicisionApp extends React.Component {
     return (
       <div>
         <Header subtitle="Put your life in the hands of a computer" />
-        <Action hasOptions={this.state.options.length > 0} onClick={this.handlePick} />
-        <Options
-          options={this.state.options}
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption onAddOption={this.handleAddOption} />
+        <div className="container">
+          <Action hasOptions={this.state.options.length > 0} onClick={this.handlePick} />
+          <Options
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption onAddOption={this.handleAddOption} />
+        </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
           handleClearSelectedOption={this.handleClearSelectedOption}
