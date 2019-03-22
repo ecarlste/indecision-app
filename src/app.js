753969ad2fc95ea9f021316d57ee2475ec64b811
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-21 22:18:53 -0600

    Began converting over to class based components

--- src/app.js
+++ src/app.js
@@ -1,57 +1,63 @@
-const app = {
-  title: 'Indicision App',
-  subtitle: 'Put your life in the hands of a computer',
-  options: []
-};
-
-const onFormSubmit = event => {
-  event.preventDefault();
-
-  const option = event.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    event.target.elements.option.value = '';
-    render();
+class IndicisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
   }
-};
+}
 
-const onRemoveAll = () => {
-  app.options = [];
-  render();
-};
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indicision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  const option = app.options[randomNum];
-  alert(option);
-};
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-      <p>{app.options.length}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
-        What should I do?
-      </button>
-      <button onClick={onRemoveAll}>Remove All</button>
+class Options extends React.Component {
+  render() {
+    return (
       <ol>
-        {app.options.map(option => (
-          <li key={option}>{option}</li>
-        ))}
+        <Option title="Item One" />
+        <Option title="Item Two" />
       </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
+    );
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return <li>{this.props.title}</li>;
+  }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <form>
+        <input />
         <button>Add Option</button>
       </form>
-    </div>
-  );
+    );
+  }
+}
 
-  const appRoot = document.getElementById('app');
-  ReactDOM.render(template, appRoot);
-};
-
-render();
+ReactDOM.render(<IndicisionApp />, document.getElementById('app'));
