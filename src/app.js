3eac713d8ed729edceee0a882b3745fe988eb087
Erik S. Carlsten <ecarlste@gmail.com>
2019-03-21 21:35:34 -0600

    First basic version of the indecision app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,57 @@
-let template = <p>this is root from app.js compiled by babel</p>;
-let appRoot = document.getElementById('app');
+const app = {
+  title: 'Indicision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
 
-ReactDOM.render(template, appRoot);
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const option = event.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    event.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>
+        What should I do?
+      </button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {app.options.map(option => (
+          <li key={option}>{option}</li>
+        ))}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+
+  const appRoot = document.getElementById('app');
+  ReactDOM.render(template, appRoot);
+};
+
+render();
